55eacd1a26f72740b0c92ba19e78640d668d3c02
Sneha <itschaudhary@gmail.com>
2025-01-22 20:37:29 +0530

    my navbar and grid css is ready

--- ecell/src/components/Grid/Grid.jsx
+++ ecell/src/components/Grid/Grid.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-
-const grid = () => {
+import React from "react";
+import "./Grid.css";
+import logo from '../assets/ecelllogo.png'
+const Body = () => {
   return (
-    <div>
-      
+    <div className="body-content"> 
+      <div className="section gradient-section">
+        
+      </div>
+      <div className="section logo-section">
+        <img
+          src={logo} 
+          alt="E-Cell Logo"
+          className="logo"
+        />
+      </div>
+      <div className="section grid-section">
+        <div className="grid-container">
+          
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default grid
+export default Body;
