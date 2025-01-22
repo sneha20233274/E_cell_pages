bfcb7ca4a6fe08dcf2b403c8aa441e7d728fa043
Sneha <itschaudhary@gmail.com>
2025-01-22 19:05:32 +0530

    navbar component

--- ecell/src/App.js
+++ ecell/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Navbar from './components/navbar/navbar.jsx'
 
 function App() {
   return (
     <div className="App">
-      hello worlld
+      <Navbar/>
     </div>
   );
 }
---
+++ ecell/src/components/navbar/navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './navbar.css'
+
+
+const navbar = () => {
+  return (
+    <div className='navbar'>
+       <ul className="nav-menu">
+        <li>Home</li>
+        <li>Grid</li>
+        <li>Linkdin</li>
+       </ul>
+      
+    </div>
+  )
+}
+
+export default navbar
